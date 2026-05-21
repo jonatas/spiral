bbf4f78ab5aa1fa1701400ea512981cc3c876b0f
Jônatas Davi Paganini <jonatasdp@gmail.com>
2026-05-21 16:56:01 -0300

    debug(tam): exhaustive notices in handler

--- src/tam.rs
+++ src/tam.rs
@@ -9,31 +9,41 @@ use pgrx::prelude::*;
 /// # Safety
 /// This function is unsafe because it interacts with PostgreSQL C internals.
 pub unsafe fn spiral_tam_handler(_fcinfo: pg_sys::FunctionCallInfo) -> pgrx::datum::Internal {
-    let routine =
-        pgrx::PgMemoryContexts::TopMemoryContext.palloc0_struct::<pg_sys::TableAmRoutine>();
+    notice!("Spiral TAM: handler started");
+    let routine = pg_sys::palloc0(std::mem::size_of::<pg_sys::TableAmRoutine>())
+        as *mut pg_sys::TableAmRoutine;
+    notice!("Spiral TAM: routine allocated at {:p}", routine);
 
     (*routine).type_ = pg_sys::NodeTag::T_TableAmRoutine;
+    notice!("Spiral TAM: type set");
 
     // Wire up the O(1) logic callbacks
     (*routine).slot_callbacks = Some(spiral_slot_callbacks);
     (*routine).tuple_insert = Some(spiral_slot_insert);
+    notice!("Spiral TAM: O(1) callbacks set");
 
     (*routine).scan_begin = Some(spiral_scan_begin);
     (*routine).scan_getnextslot = Some(spiral_scan_getnextslot);
     (*routine).scan_end = Some(spiral_scan_end);
     (*routine).scan_rescan = Some(spiral_scan_rescan);
+    notice!("Spiral TAM: scan callbacks set");
 
     (*routine).relation_size = Some(spiral_relation_size);
     (*routine).relation_estimate_size = Some(spiral_relation_estimate_size);
     (*routine).relation_set_new_filelocator = Some(spiral_relation_set_new_filelocator);
     (*routine).relation_nontransactional_truncate = Some(spiral_relation_nontransactional_truncate);
     (*routine).relation_copy_for_cluster = Some(spiral_relation_copy_for_cluster);
+    notice!("Spiral TAM: relation callbacks set");
+
     (*routine).tuple_fetch_row_version = Some(spiral_tuple_fetch_row_version);
     (*routine).tuple_tid_valid = Some(spiral_tuple_tid_valid);
     (*routine).tuple_satisfies_snapshot = Some(spiral_tuple_satisfies_snapshot);
     (*routine).relation_needs_toast_table = Some(spiral_relation_needs_toast_table);
+    notice!("Spiral TAM: tuple callbacks set");
 
-    pgrx::datum::Internal::from(Some(pg_sys::Datum::from(routine as usize)))
+    let result = pgrx::datum::Internal::from(Some(pg_sys::Datum::from(routine as usize)));
+    notice!("Spiral TAM: handler returning");
+    result
 }
 
 #[pg_guard]
